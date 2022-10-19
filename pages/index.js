8719f597e4ec4cbe55cb8c48cf966b7ee3abf4c3
Ritipu <zabya7@gmail.com>
2022-10-19 16:48:11 +0100

    Statistics

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import About from './about'
-=======
 import Buttons from "../src/components/button";
 import { MainNavBar } from "../src/components/mainNavBar";
->>>>>>> 684e03cfbd824cbd9f89c642b7559c2bce2e2f60
 
 export default function Home() {
 
   return (
-<<<<<<< HEAD
-    <div className={styles.container}>
-      < About />
-=======
 
     <div>
 
@@ -34,7 +23,6 @@ export default function Home() {
 
       </div>
 
->>>>>>> 684e03cfbd824cbd9f89c642b7559c2bce2e2f60
     </div>
 
   )
